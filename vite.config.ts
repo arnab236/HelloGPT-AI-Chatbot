61675fdf7960fdafa780fe38b5f43792390d058b
Arnab Sarkar <110808978+arnab236@users.noreply.github.com>
2025-08-24 20:54:45 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from "dotenv";
 
-dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  define: {
-    "PROCESS.ENV.API_KEY": JSON.stringify(PROCESS.ENV.API_KEY),
-  }
 });
